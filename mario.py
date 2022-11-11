05e0ace23e5627bad2f4d110efff1c6424751f51
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2022-11-10 21:38:39 -0500

    adding SARSA

--- mario.py
+++ mario.py
@@ -467,16 +467,181 @@ class MariosBros(gym.Env):
             return preprocessed_image
 
 
+def print_mario_QTable():
+    print("Mario's Q table")
+    print(Qtable_mario)
+
+def print_luigi_QTable():
+    print("Luigi's Q table")
+    print(Qtable_luigi)
+
+def randomstep():
+    list1 = [0, 1, 2, 3]
+    for i in range(0,steps):
+        #print("running")
+        res_mario = random.choice(list1)
+        res_luigi = random.choice(list1)
+        return res_mario, res_luigi
+
+
+#Q learning for mario and luigi
+def SARSA_training_algorithm(episodes):
+    
+    #----------------------SARSA TRAINING------------------------
+    
+    mariobros = MariosBros(env_type='determninistic')
+    mariobros.reset()
+    for i in range(0,episodes):
+        done_mario = False
+        done_luigi = False
+        
+        epsilon_new_val = EpsilonDecay()
+        epsilonDecayArray.append(epsilon_new_val)
+        
+        for j in range(0,40):
+            action_mario , action_luigi = randomstep()
+            observation_mario ,observation_luigi, reward_mario, reward_luigi, done_mario, done_luigi, info = (mariobros.step(action_mario, action_luigi))
+            print(observation_mario ,observation_luigi, reward_mario, reward_luigi, done_mario, done_luigi, info)
+            
+            mario_rewardArray.append(reward_mario)
+            luigi_rewardArray.append(reward_luigi)
+            
+            x_cord_mario , y_cord_mario = Get_cord_from_pos(observation_mario)
+            x_cord_luigi , y_cord_luigi = Get_cord_from_pos(observation_luigi)
+            
+            #print("coordinates for mario are:",x_cord_mario," and ", y_cord_mario)
+            #print("coordinates for luigi are:",x_cord_luigi," and ", y_cord_luigi)
+            
+            #for mario
+            Qnew_mario , state_val_mario , action_mario = Get_Q_learning_value(reward_mario, observation_mario, Qtable_mario , x_cord_mario, y_cord_mario, action_mario)
+            Qtable_mario[state_val_mario][action_mario] = Qnew_mario
+            
+            #for luigi
+            Qnew_luigi , state_val_luigi , action_luigi = Get_Q_learning_value(reward_mario, observation_mario, Qtable_mario , x_cord_mario, y_cord_mario, action_mario)
+            Qtable_luigi[state_val_luigi][action_luigi] = Qnew_luigi
+            
+            if(done_mario or done_luigi):
+                break
+            
+
+#Helper function for getting Q values
+def Get_Q_learning_value(initialreward, observation, Qtable, x_cord, y_cord, action):
+    # Q learning code
+    #Formula  - Qnew = Qold + alpha(R + gamma(Qold(max val)) - Qold)
+    
+    state_val =  Map_state_Key_Value(x_cord,y_cord)
+    Qold = Qtable [state_val] [action]
+    num = Qtable [state_val]
+    
+    Qmax = max(num)
+    Qnew = 0
+    #print("max in row:",num , " is",Qmax)
+    Qnew = Qold + alpha *(initialreward + (gamma * Qmax) - Qold)
+    #print("new Q value is:",Qnew)
+    return Qnew, state_val, action
+
+def digit_extraction_by_index(x, n):
+    return (abs(x) // (10 ** n)) % 10    
+
+def Get_cord_from_pos(carpos):
+     if(carpos==0):
+         return 0,0
+     elif(carpos==1):
+         return 0,1
+     elif(carpos==2):
+         return 0,2
+     elif(carpos==3):
+         return 0,3
+     elif(carpos==4):
+         return 0,4
+     elif(carpos==5):
+         return 0,5
+     elif(carpos==6):
+         return 0,6
+     elif(carpos==7):
+         return 0,7
+     elif(carpos==8):
+         return 0,8
+     elif(carpos==9):
+         return 0,9
+     elif(carpos==10):
+         return 1,0
+     #print("carpos is",carpos)
+     xcord = digit_extraction_by_index(carpos, 1)
+     ycord = digit_extraction_by_index(carpos, 2)
+     #print("xcord",xcord," ycord",ycord)
+     return xcord,ycord
+ 
+def getAppropiate(arr):
+    if(arr[0] == 0):
+        return 0
+    elif(arr[1] == 0):
+        return 1
+    elif(arr[2]==0):
+        return 2
+    elif(arr[3]==0):
+        return 3;
+    else:
+        return 0 #since all 4 column value must be same
+
+def Map_state_Key_Value(row, col):
+    return stateMap.get((row,col))
+
+def EpsilonDecay():
+    n = epsilonDecayArray[-1]
+    n = n * epsilon
+    epsilonDecayArray.append(n)
+    return n    
+
+
+
+#defining the state matrix
+stateMap = { (0,0):0, (0,1):1, (0,2):2, (0,3):3, (0,4):4, (0,5):5, (0,6):6, (0,7):7, (0,8):9, (0,9):10 ,  
+            (1,0):11, (1,1):12, (1,2):13, (1,3):14, (1,4):15, (1,5):16, (1,6):17, (1,7):18, (1,8):19, (1,9):20,
+            (2,0):21, (2,1):22, (2,2):23, (2,3):24, (2,4):25, (2,5):26, (2,6):27, (2,7):28, (2,8):29, (2,9):30, 
+            (3,0):31, (3,1):32, (3,2):33, (3,3):34, (3,4):35, (3,5):36, (3,6):37, (3,7):38, (3,8):39, (3,9):40, 
+            (4,0):41, (4,1):42, (4,2):43, (4,3):44, (4,4):45, (4,5):46, (4,6):47, (4,7):48, (4,8):49, (4,9):50, 
+            (5,0):51, (5,1):52, (5,2):53, (5,3):54, (5,4):55, (5,5):56, (5,6):57, (5,7):58, (5,8):59, (5,9):60, 
+            (6,0):61, (6,1):62, (6,2):63, (6,3):64, (6,4):65, (6,5):66, (6,6):67, (6,7):68, (6,8):69, (6,9):70,
+            (7,0):71, (7,1):72, (7,2):73, (7,3):74, (7,4):75, (7,5):76, (7,6):77, (7,7):78, (7,8):79, (7,9):80, 
+            (8,0):81, (8,1):82, (8,2):83, (8,3):84, (8,4):85, (8,5):86, (8,6):87, (8,7):88, (8,8):89, (8,9):90, 
+            (9,0):91, (9,1):92, (9,2):93, (9,3):94, (9,4):95, (9,5):96, (9,6):97, (9,7):98, (9,8):99, (9,9):100
+            }
+
+
+
+#defining the hyper-parameters for Q learning algorithm
+states  = 100 # we have 10*10 grid
+rows = 10
+cols = 10
+actions = 4 # agent can perform 4 actions left,right,up,down
+alpha = 0.2 
+gamma = 0.8
+epsilon = 0.95 
+epochs = 10000
+steps = 40 #by default in each episode , the agent will try to run for 40 steps
+
+Qtable_mario = np.zeros((100, 4))
+Qtable_luigi = np.zeros((100, 4))
+
+epsilonDecayArray = []
+
+epsilonDecayArray.append(0.98)
+
+mario_rewardArray = []
+luigi_rewardArray = []
+    
 #main driver logic
-env_num = int(input('Please select type of environment: 1-deterministic and 2-stochastic :'))
+env_num = int(input('Please select type of environment: 1-deterministic and 2-stochastic 3-Sarsa-multiagent:'))
 print("Environment chosen is:",env_num)
 
-steps = int(input('Please enter the number of steps you want to take :'))
-print("The agent will take",steps,"steps now")
 
 
 if env_num == 1:
     #code for deterministic environment
+    steps = int(input('Please enter the number of steps you want to take :'))
+    print("The agent will take",steps,"steps now")
+
     mariobros = MariosBros(env_type='deterministic')
     mariobros.reset()
     # creating a list of actions - up,down,right,left - each has 0.25 probability
@@ -491,38 +656,49 @@ if env_num == 1:
 elif env_num == 2:
     #code for stochastic environment
     mariobros = MariosBros(env_type='stochastic')
+    steps = int(input('Please enter the number of steps you want to take :'))
+    print("The agent will take",steps,"steps now")
+
     mariobros.reset()
     for i in range(0,steps):
         print("running")
         step_to_take = (random.randint(1, 100))
         if(step_to_take<90):
             if(random.choice([1, 2]) == 1):
-                action = 0
+                action_mario = 0
+                action_luigi = 1
                 #print("stochastic choice by env is: ", action)
-                print(mariobros.step(action))
+                print(mariobros.step(action_mario, action_luigi))
                 mariobros.render(plot=True)
             else:
-                action = 2
+                action_mario = 2
+                action_luigi = 2
                 #print("stochastic choice by env is: ", action)
-                print(mariobros.step(action))
+                print(mariobros.step(action_mario, action_luigi))
                 mariobros.render(plot=True)
         else:
             if(random.choice([1, 2]) == 1):
-                action = 1
+                action_mario = 1
+                action_luigi = 0
                 #print("stochastic choice by env is: ", action)
-                print(mariobros.step(action))
+                print(mariobros.step(action_mario, action_luigi))
                 mariobros.render(plot=True)
             else:
-                action = 3
+                action_mario = 3
+                action_luigi = 3
                 #print("stochastic choice by env is: ", action)
-                print(mariobros.step(action))
+                print(mariobros.step(action_mario, action_luigi))
                 mariobros.render(plot=True)
+elif env_num==3:
+    episodes = int(input('Please enter the number of episodes:'))
+    print("Running the agent for ",episodes," now")
+
+    SARSA_training_algorithm(episodes)
+    print_mario_QTable()
+    print_luigi_QTable()
 else:
     print('Invalid choice')
 
-
-    
-    
 
 print("END OF EXECUTION") 
 
